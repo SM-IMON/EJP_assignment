1bed9bbbab837498f612d06aa7005e90dd10194e
imonDVP <imon.dvp@gmail.com>
2025-01-24 21:12:24 +0600

    My code is updated

--- src/app/blog/[blogid]/page.jsx
+++ src/app/blog/[blogid]/page.jsx
@@ -1,14 +1,19 @@
 
 
+const page = async({params}) => {
+const {blogid}=params
 
-import React from 'react'
-
-const page = ({params}) => {
-
+const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${blogid}`);
+const post = await res.json();
+console.log(post)
+const {userId,id,title,body}=post
+if (!post) return <div className="p-8">Loading...</div>;
   return (
-    <div>
-      {params.blogid}
-      <h2>I am page</h2>
+    <div className="flex flex-col justify-start  w-[700px] mx-auto py-12">
+      <h2> <span>Title : </span>{title}</h2>
+      <p><span>id : </span> {id}</p>
+      <p><span>UserId : </span> {userId}</p>
+      <p><span>Description : </span> {body}</p>
     </div>
   )
 }
--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,9 +1,21 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 
 
-const ProfilePage = () => {
+const ProfilePage = async() => {
+    const {getUser}=getKindeServerSession()
+    const user=await getUser()
+    console.log(user)
+const {email,family_name}=user
+  
   return (
-    <div>
-        <h2>Profile page</h2>
+    <div className="card bg-primary text-primary-content w-[500px] mx-auto mt-16">
+      <div className="card-body">
+        <p> <span>Familt name : </span>{family_name}</p>
+        <h2 className="card-title">{email}</h2>
+        <div className="card-actions justify-end">
+
+        </div>
+      </div>
     </div>
   )
 }
